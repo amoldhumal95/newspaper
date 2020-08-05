5164ce809827a348f2fcacce927c02276d0889ff
amoldhumal95 <54957066+amoldhumal95@users.noreply.github.com>
2020-08-05 17:18:12 +0530

    Update master.js

--- master.js
+++ master.js
@@ -56,3 +56,19 @@ function read() {
       moreText.style.display = "inline";
     }
   }
+
+  function read2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
